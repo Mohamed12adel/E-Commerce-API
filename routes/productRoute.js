f470bd1489974b4679379acfaff964e2a4ccc930
Mohamed12adel <162705350+Mohamed12adel@users.noreply.github.com>
2026-05-14 16:33:00 +0300

    first commit

---
+++ routes/productRoute.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const {
+  getProductValidator,
+  createProductValidator,
+  updateProductValidator,
+  deleteProductValidator,
+} = require("../utils/validators/productValidator");
+const {
+  getProducts,
+  getProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  uploadProductImages,
+  resizeProductImages,
+} = require("../controllers/productController");
+const authService = require("../controllers/authController");
+const reviewsRoute = require("./reviewRoute");
+const router = express.Router();
+router.use("/:productId/reviews", reviewsRoute);
+router
+  .route("/")
+  .get(getProducts)
+  .post(
+    authService.protect,
+    authService.allowedTo("admin", "manager"),
+    uploadProductImages,
+    resizeProductImages,
+    createProductValidator,
+    createProduct,
+  );
+router
+  .route("/:id")
+  .get(getProductValidator, getProduct)
+  .put(
+    authService.protect,
+    authService.allowedTo("admin", "manager"),
+    uploadProductImages,
+    resizeProductImages,
+    updateProductValidator,
+    updateProduct,
+  )
+  .delete(
+    authService.protect,
+    authService.allowedTo("admin"),
+    deleteProductValidator,
+    deleteProduct,
+  );
+module.exports = router;
